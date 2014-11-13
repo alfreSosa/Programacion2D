99eb3246569f65646f885fa922604044a52cafd6
Alfredo Sosa <alfres92@gmail.com>
2014-11-13 14:26:52 +0100

    Animación circulo acabada. Falta salida por escape y revisar.

--- Motor2D/main.cpp
+++ Motor2D/main.cpp
@@ -11,25 +11,43 @@ int main(int argc, char* argv[]) {
 
   const float rectCenterTam = 50.0f;
   const int rectMouseTam = 10;
+  const int circleMouseRadius = 5;
 
   const float midRectCenterTam = rectCenterTam / 2;
   const int midRectMouseTam = rectMouseTam / 2;
-
+  const int circleDistanciaAlMouse = 30;
+  const int posicionFinal = 360;
   int x_mouse;
   int y_mouse;
 
+  float x_mouseCircle;
+  float y_mouseCircle;
+
   float x_midle = SCREEN.GetWidth() / 2.0f - midRectCenterTam;
   float y_midle = SCREEN.GetHeight() / 2.0f - midRectCenterTam;
-  
-  while ( SCREEN.IsOpened() ) 
+
+  float posCircle = 0.0f;
+  float senoCircle = 0.0f;
+  float cosenoCircle = 1.0f;
+  /*Preguntar como esconder el cursor*/
+
+  while ( SCREEN.IsOpened() /*TAREA: SALIR CON ESC*/) 
   {
     //Obtener posisicion raton
     x_mouse = SCREEN.GetMouseX() - midRectMouseTam;
     y_mouse = SCREEN.GetMouseY() - midRectMouseTam;
 
+    //Calcular posicion circulo
+    posCircle += SCREEN.ElapsedTime() * posicionFinal; 
+    senoCircle =  DegSin(-posCircle); // negativo para sentido antihorario.
+    cosenoCircle =  DegCos(posCircle); 
+
+    x_mouseCircle = x_mouse + cosenoCircle * circleDistanciaAlMouse;
+    y_mouseCircle = y_mouse + senoCircle * circleDistanciaAlMouse;
+
     //Obtener angulo y distancia
-    float angulo = Angle(x_mouse - 10, y_mouse - 10, x_mouse ,y_mouse);
-    float distance = Distance(x_mouse + (rectMouseTam / 2),y_mouse + (rectMouseTam / 2),x_midle + midRectCenterTam, y_midle + midRectCenterTam);
+    float angulo = Angle(x_mouseCircle, y_mouseCircle, x_mouse ,y_mouse);
+    float distance = Distance(x_mouse + midRectMouseTam, y_mouse + midRectMouseTam, x_midle + midRectCenterTam, y_midle + midRectCenterTam);
 
     //pintar titulo
     String tit1 = "Grados: ";
@@ -50,7 +68,7 @@ int main(int argc, char* argv[]) {
     //Fijar color azul, pintar elipse en...
     RENDER.SetColor(0,0,255,0);
     SCREEN.ElapsedTime();
-    RENDER.DrawEllipse(x_mouse - 10, y_mouse - 10, 5 ,5);
+    RENDER.DrawEllipse(x_mouseCircle, y_mouseCircle, circleMouseRadius , circleMouseRadius);
 		// Refrescamos la pantalla
 		SCREEN.Refresh();
     RENDER.Clear(0,0,0);
